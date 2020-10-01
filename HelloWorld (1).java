e7bf9d9d4b21c2a014efb30c9c0f6f9630c30832
abhinavawasthi1729 <56489028+abhinavawasthi1729@users.noreply.github.com>
2020-10-01 23:27:04 +0530

    Update HelloWorld (1).java

--- HelloWorld (1).java
+++ HelloWorld (1).java
@@ -1,30 +1,20 @@
- class HelloWorld{
+ class BubbleSort{					//this class name explains the whole programme :) 
 
 	public static void main(String args[]){
-	
 	int a[]={1,4,3,2,5,7,6,9,10,8};
-	for(int i=0;i<a.length;i++){
-
-		for(int j=0;j<a.length-1;j++){
-
-		if(a[j]>a[j+1]){
-
-		int temp=a[j];
-		a[j]=a[j+1];
-		a[j+1]=temp;
-		
+	for(int i=0;i<a.length-1;i++){                  
+		for(int j=i;j<a.length-1;j++){		//j should begin with j=i
+			if(a[j]>a[j+1]){
+				int temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
 		}
-	}
-	
-
-	} 
-
- 
+	}  
 	for(int i=0;i<a.length;i++){
-	
-	System.out.print(a[i]+"\n");	
+		System.out.print(a[i]+"\n");	
 	}  
 
-	}
+   }
 
 }
